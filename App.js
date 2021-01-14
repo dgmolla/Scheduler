1cb2ea92cc51ab769ae6370b913635b30a633792
Dawit Molla <dgmolla@gmail.com>
2021-01-14 17:37:18 -0600

    Course list

--- App.js
+++ App.js
@@ -1,15 +1,60 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaView, ScrollView, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 
 const schedule = {
-  title: "CS Courses for 2018-2019"
+  title: "CS Courses for 2018-2019",
+  courses: [
+    {
+      "id": "F101",
+      "title": "Computer Science: Concepts, Philosophy, and Connections",
+      "meets": "MWF 11:00-11:50"
+    },
+    {
+      "id": "F110",
+      "title": "Intro Programming for non-majors",
+      "meets": "MWF 10:00-10:50"
+    },
+    {
+      "id": "F111",
+      "title": "Fundamentals of Computer Programming I",
+      "meets": "MWF 13:00-13:50"
+    },
+    {
+      "id": "F211",
+      "title": "Fundamentals of Computer Programming II",
+      "meets": "TuTh 12:30-13:50"
+    }
+  ]
 };
+
+const Banner = ({title}) => (
+  <Text style={styles.bannerStyle}>{title}</Text>
+);
+
+const getCourseNumber = course => (
+  course.id.slice(1)
+);
+
+const Course = ({course}) => (
+  <TouchableOpacity style={styles.courseButton}>
+    <Text style={styles.courseText}>
+      {`CS ${getCourseNumber(course)}\n${course.meets}`}
+    </Text>
+  </TouchableOpacity>
+);
+
+const CourseList = ({courses}) => (
+  <ScrollView style={styles.courseList}>
+    {courses.map(course => <Course key={course.id} course={course} />)}
+  </ScrollView>
+);
 
 const App = () => {
   return (
-    <View style={styles.container}>
-      <Text style={styles.bannerStyle}>{schedule.title}</Text>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <Banner title={schedule.title} \>
+      <CourseList courses={schedule.courses} \>
+    </SafeAreaView>
   );
 }
 
@@ -23,7 +68,31 @@ const styles = StyleSheet.create({
   bannerStyle: {
     color: '#888',
     fontSize: 32,
-  }
+  },
+  courseList: {
+    flex: 1,
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+  },
+  courseButton: {
+    flex: 1,
+    borderRadius: 5,
+    justifyContent: 'center',
+    alignItems: 'center',
+    margin: 10,
+    height: 60,
+    padding: 10,
+    minWidth: 90,
+    maxWidth: 90,
+    backgroundColor: '#66b0ff',
+  },
+  courseText:{
+    color: '#fff',
+    fontSize: 12,
+    textAlign: 'center',
+  },
 });
 
 export default App;
