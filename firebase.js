e76bcb3ab9b79647710b1ca09ea7ddf509489cf6
Dawit Molla <dgmolla@gmail.com>
2021-01-31 19:35:52 -0600

    get data from firebase

---
+++ firebase.js
@@ -0,0 +1,17 @@
+import * as firebase from 'firebase';
+
+import "firebase/database";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyACyiFXKVlvuUwgz9KcPROgGp0YRDZmq_o",
+  authDomain: "selector-680f5.firebaseapp.com",
+  databaseURL: "https://selector-680f5-default-rtdb.firebaseio.com",
+  projectId: "selector-680f5",
+  storageBucket: "selector-680f5.appspot.com",
+  messagingSenderId: "822440709416",
+  appId: "1:822440709416:web:b4923846c84c830513941d"
+};
+
+firebase.initializeApp(firebaseConfig);
+
+export { firebase };
--- screens/scheduleScreen.js
+++ screens/scheduleScreen.js
@@ -1,9 +1,16 @@
 import React, { useContext, useState, useEffect } from 'react';
 import CourseEditScreen from './courseEditScreen';
-
+import {firebase} from '../firebase.js'
 import CourseList from '../components/courseList';
 import UserContext from '../userContext.js';
 import { SafeAreaView, View, StyleSheet, Text } from 'react-native';
+
+const db = firebase.database().ref();
+
+const fixCourses = json => ({
+  ...json,
+  courses: Object.values(json.courses)
+});
 
 const Banner = ({title}) => (
   <Text style={styles.bannerStyle}>{title || '[loading...]'}</Text>
@@ -14,21 +21,20 @@ const ScheduleScreen = ({navigation}) => {
   const canEdit = user && user.role == 'admin';
 
   const [schedule, setSchedule] = useState({ title: '', courses: [] });
-  const url = 'https://courses.cs.northwestern.edu/394/data/cs-courses.php';
+  //const url = 'https://courses.cs.northwestern.edu/394/data/cs-courses.php';
 
   const view = (course) => {
     navigation.navigate(canEdit ? 'CourseEditScreen' : 'CourseDetailScreen', { course });
   };
 
   useEffect(() => {
-    const fetchSchedule = async () => {
-      const response = await fetch(url);
-      if (!response.ok) throw response;
-      const json = await response.json();
-      setSchedule(json);
-    };
-    fetchSchedule();
-  }, [])
+    const db = firebase.database().ref();
+    const handleData = snap => {
+      if (snap.val()) setSchedule(fixCourses(snap.val()));
+    }
+    db.on('value', handleData, error => alert(error));
+    return () => { db.off('value', handleData); };
+  }, []);
 
   return (
     <SafeAreaView style={styles.container}>
